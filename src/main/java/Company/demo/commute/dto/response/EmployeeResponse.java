2c2bdb591379050291337bb1d7b2ae7d5fc7b871
dong_hy <kimadonghyon99@gmail.com>
2024-03-03 23:03:29 +0900

    feat : DTO 직원 조회 응답객체

---
+++ src/main/java/Company/demo/commute/dto/response/EmployeeResponse.java
@@ -0,0 +1,14 @@
+package Company.demo.commute.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeResponse {
+    String name;
+    String teamName;
+    String role;
+    String birthDay;
+    String WorkStartDate;
+}
