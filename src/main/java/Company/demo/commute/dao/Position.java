d2f31449e5c6898a7a3bd8b8e46e2357abe11a7c
dong_hy <kimadonghyon99@gmail.com>
2024-03-03 22:59:52 +0900

    feat : DAO 직급 객체

--- src/main/java/Company/demo/commute/dao/Position.java
+++ src/main/java/Company/demo/commute/dao/Position.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Position {
@@ -12,6 +14,22 @@ public class Position {
     private Long positionId;
     private String positionName;
 
+
+    @OneToMany(mappedBy = "position")
+    private List<Employee> employeeList;
+
     public Position() {
+    }
+
+    public Position(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public Long getPositionId() {
+        return positionId;
+    }
+
+    public String getPositionName() {
+        return positionName;
     }
 }
