bfed1588782e693591d6aae3709e8fcb9fa89536
dong_hy <kimadonghyon99@gmail.com>
2024-03-03 22:57:09 +0900

    feat : DAO Team객체 수정 1. Employee 객체와 관계추가 get 2. getter 추가 3. emplyee를 찾아서 리턴해주는 메서드 추가

--- src/main/java/Company/demo/commute/dao/Team.java
+++ src/main/java/Company/demo/commute/dao/Team.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -11,12 +13,37 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long teamId;
     private String teamName;
-    private String teamLeaderId;
+    private Integer teamLeaderId;
+
+    @OneToMany(mappedBy = "team")
+    private List<Employee> employeeList;
 
     public Team() {
     }
 
     public Team(String positionName) {
         this.teamName = positionName;
+    }
+
+    public void setTeamLeader(Integer leaderCode) {
+        this.teamLeaderId = leaderCode;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public Integer getTeamLeaderId() {
+        return teamLeaderId;
+    }
+
+    public Employee findEmployee(String userName) {
+        return employeeList.stream().filter(employee -> employee.getName().equals(userName))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
