90a8ab43db40cba6a74d9d8327c5b54ce6df609e
dong_hy <kimadonghyon99@gmail.com>
2024-03-03 23:02:47 +0900

    feat : DAO 직원객체 1. Team(부서)객체와 ManyToOne관계 2. Position(직급)객체와 ManyToOne관계 3. CommuteHistory(출퇴근기록)와 OneToMany관계

--- src/main/java/Company/demo/commute/dao/Employee.java
+++ src/main/java/Company/demo/commute/dao/Employee.java
@@ -1,29 +1,59 @@
 package Company.demo.commute.dao;
 
+import Company.demo.commute.dto.request.MemberSaveRequest;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
     @ManyToOne
+    @JoinColumn(name = "position_id")
     private Position position;
+
     @ManyToOne
+    @JoinColumn(name = "team_id")
     private Team team;
 
-    public Employee() {}
+    @OneToMany(mappedBy = "employee")
+    private List<CommuteHistory> commuteHistories;
 
-    public Employee(Long id, String name, Position position, Team team) {
-        this.id = id;
-        this.name = name;
+    private String startDate;
+    private String birthDay;
+
+
+    public Employee() {
+    }
+
+    public Employee(MemberSaveRequest request, Position position) {
+        this.name = request.getName();
         this.position = position;
+        this.startDate = request.getStartDate();
+        this.birthDay = request.getBirthDay();
+    }
+
+    public void setTeam(Team team) {
         this.team = team;
     }
+
+    public Optional<CommuteHistory> findCommuteHistory() {
+        return commuteHistories.stream()
+                .filter(target -> target.getStartTime().toLocalDate().equals(LocalDateTime.now().toLocalDate()))
+                .findFirst();
+    }
+
 }
