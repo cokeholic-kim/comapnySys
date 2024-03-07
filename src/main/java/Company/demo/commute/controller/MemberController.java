b82d3d983fa7e11087af61498424dede79bbd200
dong_hy <kimadonghyon99@gmail.com>
2024-03-07 19:16:38 +0900

    feat : 연차,연장근무관련 API추가

--- src/main/java/Company/demo/commute/controller/MemberController.java
+++ src/main/java/Company/demo/commute/controller/MemberController.java
@@ -1,9 +1,14 @@
 package Company.demo.commute.controller;
 
+import Company.demo.commute.dto.request.ALRequest;
 import Company.demo.commute.dto.request.MemberSaveRequest;
 import Company.demo.commute.dto.request.MemberTeamSetDto;
+import Company.demo.commute.dto.request.OverTimeRequest;
 import Company.demo.commute.dto.response.EmployeeResponse;
+import Company.demo.commute.dto.response.OverTimeResponse;
+import Company.demo.commute.service.ExcelService;
 import Company.demo.commute.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService service;
+    private final ExcelService excelService;
 
     @PostMapping
     public void saveMember(@RequestBody MemberSaveRequest request) {
@@ -25,12 +31,32 @@ public class MemberController {
     }
 
     @PostMapping("/setTeam")
-    public void setTeam(@RequestBody MemberTeamSetDto request){
+    public void setTeam(@RequestBody MemberTeamSetDto request) {
         service.setTeam(request);
     }
 
     @GetMapping
-    public List<EmployeeResponse> getEmployee(){
+    public List<EmployeeResponse> getEmployee() {
         return service.getEmployee();
+    }
+
+    @PostMapping("/register/annualLeave")
+    public void registerAL(@RequestBody ALRequest request) {
+        service.registerAl(request);
+    }
+
+    @GetMapping("/annualLeave")
+    public Integer getAnnualLeave(Integer employeeId) {
+        return service.getAnnualLeave(employeeId);
+    }
+
+    @PostMapping("/overTimeCalc")
+    public List<OverTimeResponse> getOverTime(@RequestBody OverTimeRequest overTimeRequest) {
+        return service.getOverTime(overTimeRequest.getYearMonth(), overTimeRequest.getHolidays());
+    }
+
+    @GetMapping("/overTimeExcel")
+    public void getExcel(Integer holidays, HttpServletResponse response) {
+        excelService.getExcel(holidays, response);
     }
 }
--- src/main/java/Company/demo/commute/service/MemberService.java
+++ src/main/java/Company/demo/commute/service/MemberService.java
@@ -1,14 +1,28 @@
 package Company.demo.commute.service;
 
+import Company.demo.commute.dao.AnnualLeaveRegister;
+import Company.demo.commute.dao.CommuteHistory;
+import Company.demo.commute.dao.CompanyPolicy;
 import Company.demo.commute.dao.Employee;
 import Company.demo.commute.dao.Position;
 import Company.demo.commute.dao.Team;
+import Company.demo.commute.dto.request.ALRequest;
 import Company.demo.commute.dto.request.MemberSaveRequest;
 import Company.demo.commute.dto.request.MemberTeamSetDto;
 import Company.demo.commute.dto.response.EmployeeResponse;
+import Company.demo.commute.dto.response.OverTimeResponse;
+import Company.demo.commute.repository.AnnualLeaveRepository;
+import Company.demo.commute.repository.CommuteHistoryRepository;
+import Company.demo.commute.repository.CompanyPolicyRepository;
 import Company.demo.commute.repository.MemberRepository;
 import Company.demo.commute.repository.PositionRepository;
 import Company.demo.commute.repository.TeamRepository;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,6 +37,10 @@ public class MemberService {
     private final MemberRepository repository;
     private final PositionRepository positionRepository;
     private final TeamRepository teamRepository;
+    private final AnnualLeaveRepository AlRepository;
+    private final CommuteHistoryRepository commuteRepository;
+    private final CompanyPolicyRepository policyRepository;
+
     @Transactional
     public void saveMember(MemberSaveRequest request) {
 
@@ -30,6 +48,7 @@ public class MemberService {
 
         repository.save(new Employee(request, position));
     }
+
     @Transactional
     public void setTeam(MemberTeamSetDto request) {
         Employee target = repository.findByName(request.getEmployeeName());
@@ -40,14 +59,84 @@ public class MemberService {
 
     public List<EmployeeResponse> getEmployee() {
         return repository.findAll().stream()
-                .map(result->{
+                .map(result -> {
                     EmployeeResponse response = new EmployeeResponse();
                     response.setName(result.getName());
                     response.setBirthDay(result.getBirthDay());
-                    response.setRole(Optional.ofNullable(result.getPosition()).map(Position::getPositionName).orElse(""));
+                    response.setRole(
+                            Optional.ofNullable(result.getPosition()).map(Position::getPositionName).orElse(""));
                     response.setTeamName(Optional.ofNullable(result.getTeam()).map(Team::getTeamName).orElse(""));
                     response.setWorkStartDate(result.getStartDate());
                     return response;
                 }).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void registerAl(ALRequest request) {
+        Employee employee = repository.findById(request.getEmployeeId()).orElseThrow(IllegalArgumentException::new);
+        Team team = teamRepository.findById(employee.getTeam().getTeamId()).orElseThrow(IllegalArgumentException::new);
+        Integer annualLeaveBefore = team.getAnnualLeaveBefore();
+        long between = ChronoUnit.DAYS.between(LocalDate.now(), request.getStartDate());
+        if (between < 0 || between < annualLeaveBefore) {
+            System.out.print("휴가 신청이 불가능합니다.");
+        } else {
+            AlRepository.save(new AnnualLeaveRegister(employee, request.getStartDate(), request.getEndDate()));
+            employee.useAL((int) ChronoUnit.DAYS.between(request.getStartDate(), request.getEndDate()) + 1);
+        }
+    }
+
+    public Integer getAnnualLeave(Integer employeeId) {
+        Employee employee = repository.findById(Long.valueOf(employeeId)).orElseThrow(IllegalArgumentException::new);
+        return employee.getAnnualLeave();
+    }
+
+    @Transactional
+    public List<OverTimeResponse> getOverTime(String yearMonth, Integer holidays) {
+        CompanyPolicy policy = policyRepository.findByPolicyGubnAndPolicyName("workTime", "dailyWorkLimit");
+        int standardWorkHour = calcStandardWorkDays(yearMonth, holidays) * Integer.parseInt(policy.getPolicyContent());
+        List<Employee> employeeList = repository.findAll();
+        List<OverTimeResponse> collect = employeeList.stream().map(result -> {
+            OverTimeResponse response = new OverTimeResponse();
+            response.setId(Math.toIntExact(result.getId()));
+            response.setName(result.getName());
+            System.out.println("result.getName() = " + result.getName());
+            response.setOvertimeMinutes(
+                    getOverTime(calcEmployeeWorkHour(result.getCommuteHistories(), yearMonth), standardWorkHour));
+            return response;
+        }).collect(Collectors.toList());
+        return collect;
+    }
+
+    private Integer getOverTime(Integer workTime, Integer standardWorkHour) {
+        System.out.println("workTime = " + workTime);
+        System.out.println("standardWorkHour = " + standardWorkHour);
+        Integer standardWorkMinute = standardWorkHour * 60;
+        if (workTime > standardWorkMinute) {
+            return workTime - standardWorkMinute;
+        } else {
+            return 0;
+        }
+    }
+
+    private Integer calcEmployeeWorkHour(List<CommuteHistory> commuteHistories, String yearMonth) {
+        return commuteHistories.stream().filter(result -> isSameYearMonth(yearMonth, result))
+                .mapToInt(result -> diffMinute(result.getStartTime(), result.getEndTime()))
+                .sum();
+    }
+
+    private Integer diffMinute(LocalDateTime start, LocalDateTime end) {
+        Duration duration = Duration.between(start, end);
+        return (int) duration.toMinutes();
+    }
+
+    private static boolean isSameYearMonth(String yearMonth, CommuteHistory queryResult) {
+        YearMonth yearMonthFromQuery = YearMonth.from(queryResult.getStartTime().toLocalDate());
+        YearMonth yearMonthFromRequest = YearMonth.parse(yearMonth, DateTimeFormatter.ofPattern("yyyy-MM"));
+        return yearMonthFromQuery.equals(yearMonthFromRequest);
+    }
+
+    private Integer calcStandardWorkDays(String yearMonth, Integer holidays) {
+        int daysInMonth = YearMonth.parse(yearMonth, DateTimeFormatter.ofPattern("yyyy-MM")).lengthOfMonth();
+        return daysInMonth - holidays;
     }
 }
