297f75b41bd0c5a2bb7ed78bad9ebfc31e37060a
dong_hy <kimadonghyon99@gmail.com>
2024-03-10 00:04:34 +0900

    refactor : TeamRepository수정 1. 팀조회 jpql로 변경 2. 팀 이름 조회(true , false)추가

--- src/main/java/Company/demo/commute/repository/TeamRepository.java
+++ src/main/java/Company/demo/commute/repository/TeamRepository.java
@@ -1,18 +1,20 @@
 package Company.demo.commute.repository;
 
 import Company.demo.commute.dao.Team;
+import Company.demo.commute.dto.response.TeamResponse;
 import java.util.List;
-import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface TeamRepository extends JpaRepository<Team,Long> {
+public interface TeamRepository extends JpaRepository<Team, Long> {
     Team findByTeamName(String teamName);
 
-    @Query(value = "SELECT A.team_name AS name, B.name AS manager, COUNT(C.id) AS count " +
-            "FROM team AS A " +
-            "LEFT JOIN employee AS B ON A.team_leader_id = B.id " +
-            "LEFT JOIN employee AS C ON A.team_id = C.team_id " +
-            "GROUP BY A.team_name, B.name", nativeQuery = true)
-    List<Map<String, Object>> findTeamDetailsWithManagerAndCount();
+    Boolean existsByTeamName(String teamName);
+
+    @Query("SELECT new Company.demo.commute.dto.response.TeamResponse(a.teamName, b.name, COUNT(c.id)) " +
+            "FROM Team a " +
+            "LEFT JOIN Employee b ON a.teamLeader = b " +
+            "LEFT JOIN Employee c ON a.teamId = c.team.teamId " +
+            "GROUP BY a.teamName, b.name")
+    List<TeamResponse> getAllTeam();
 }
