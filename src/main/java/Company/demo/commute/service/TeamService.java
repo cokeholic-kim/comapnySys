ac75e426edc5ba92b5221fc13915f15ad8872f37
dong_hy <kimadonghyon99@gmail.com>
2024-03-10 00:07:33 +0900

    refactor : TeamService수정 1. isExistTeam추가 -팀 유무확인 2. 팀조회 수정 getTeam 3. 팀장 등록 수정.

--- src/main/java/Company/demo/commute/service/TeamService.java
+++ src/main/java/Company/demo/commute/service/TeamService.java
@@ -1,44 +1,42 @@
 package Company.demo.commute.service;
 
 import Company.demo.commute.dao.Team;
-import Company.demo.commute.dto.request.MemberTeamSetDto;
 import Company.demo.commute.dto.request.TeamSetDto;
 import Company.demo.commute.dto.response.TeamResponse;
+import Company.demo.commute.repository.MemberRepository;
 import Company.demo.commute.repository.TeamRepository;
-import java.math.BigInteger;
 import java.util.List;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class TeamService {
-
     private final TeamRepository repository;
+    private final MemberRepository memberRepository;
 
-    public TeamService(TeamRepository repository) {
-        this.repository = repository;
-    }
-
+    @Transactional
     public void registerTeam(String teamName) {
+        if (isExistTeam(teamName)) {
+            throw new IllegalArgumentException("이미 존재하는 팀 이름입니다: " + teamName);
+        }
         repository.save(new Team(teamName));
     }
 
     @Transactional
     public void registerTeamLeader(TeamSetDto request) {
         Team team = repository.findByTeamName(request.getTeamName());
-        team.setTeamLeader(request.getLeader_code());
+        team.setTeamLeader(memberRepository.findById(Long.valueOf(request.getLeaderCode()))
+                .orElseThrow(IllegalArgumentException::new));
     }
 
+    @Transactional
     public List<TeamResponse> getTeam() {
-        return repository.findTeamDetailsWithManagerAndCount().stream()
-                .map(result ->{
-                    TeamResponse response = new TeamResponse();
-                    response.setName((String) result.get("name"));
-                    response.setManager((String) result.get("manager"));
-                    response.setCount((long)result.get("count"));
-                    return response;
-                })
-                .collect(Collectors.toList());
+        return repository.getAllTeam();
+    }
+
+    private boolean isExistTeam(String name) {
+        return repository.existsByTeamName(name);
     }
 }
