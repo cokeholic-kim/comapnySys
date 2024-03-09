d92ca2b3728748390b011adfcb09256c6023d3a5
dong_hy <kimadonghyon99@gmail.com>
2024-03-10 00:01:35 +0900

    docs : readme추가 refactor : Entity수정 1. 팀장 코드 관계설정. teamLeaderCode -> teamLeader

--- src/main/java/Company/demo/commute/dao/Team.java
+++ src/main/java/Company/demo/commute/dao/Team.java
@@ -4,37 +4,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long teamId;
     private String teamName;
-    private Integer teamLeaderId;
+    @OneToOne
+    @JoinColumn(name = "team_leader_id")
+    private Employee teamLeader;
     private Integer annualLeaveBefore;
 
     @OneToMany(mappedBy = "team")
     private List<Employee> employeeList;
 
-    public Team() {
-    }
-
     public Team(String positionName) {
         this.teamName = positionName;
     }
 
-    public void setTeamLeader(Integer leaderCode) {
-        this.teamLeaderId = leaderCode;
-    }
-
-    public Employee findEmployee(String userName) {
-        return employeeList.stream().filter(employee -> employee.getName().equals(userName))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+    public void setTeamLeader(Employee leader) {
+        this.teamLeader = leader;
     }
 }
