f9c59d78b8f09eaa0aa98be1a9a7d47823510a3b
dong_hy <kimadonghyon99@gmail.com>
2024-03-19 23:22:55 +0900

    refactor : 직급엔티티 1. 어노테이션추가

--- src/main/java/Company/demo/commute/dao/Position.java
+++ src/main/java/Company/demo/commute/dao/Position.java
@@ -6,30 +6,21 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Position {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long positionId;
     private String positionName;
-
-
     @OneToMany(mappedBy = "position")
     private List<Employee> employeeList;
 
-    public Position() {
-    }
-
     public Position(String positionName) {
         this.positionName = positionName;
-    }
-
-    public Long getPositionId() {
-        return positionId;
-    }
-
-    public String getPositionName() {
-        return positionName;
     }
 }
