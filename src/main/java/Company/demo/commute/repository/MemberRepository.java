97c79ed45c12c9067384df16e0294c3485c3ba93
dong_hy <kimadonghyon99@gmail.com>
2024-03-30 19:35:11 +0900

    refactor : 엑셀서비스 파라미터수정.

--- src/main/java/Company/demo/commute/repository/MemberRepository.java
+++ src/main/java/Company/demo/commute/repository/MemberRepository.java
@@ -3,6 +3,14 @@ package Company.demo.commute.repository;
 import Company.demo.commute.dao.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Employee,Long> {
+public interface MemberRepository extends JpaRepository<Employee, Long> {
     Employee findByName(String name);
+
+//    @Query("SELECT new Company.demo.commute.dto.response.OverTimeResponse(a.id, a.name, SUM(function('TIMESTAMPDIFF', b.startTime, b.endTime) )) " +
+//            "FROM Employee a " +
+//            "LEFT outer JOIN CommuteHistory b ON a = b.employee " +
+//            "WHERE b.startTime >= :startDate AND b.startTime <= :endDate " +
+//            "GROUP BY a")
+//    List<OverTimeResponse> getAllByYearMonthOverTimeResponse(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 }
--- src/main/java/Company/demo/commute/service/ExcelService.java
+++ src/main/java/Company/demo/commute/service/ExcelService.java
@@ -1,11 +1,13 @@
 package Company.demo.commute.service;
 
+import Company.demo.commute.dto.request.OverTimeRequest;
 import Company.demo.commute.dto.response.OverTimeResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,72 +23,80 @@ public class ExcelService {
     private final MemberService memberService;
 
     public void getMemberExcel(Integer holidays,HttpServletResponse response){
-        List<OverTimeResponse> overTime = memberService.getOverTime(getPreviousMonth(), holidays);
+        OverTimeRequest build = OverTimeRequest.builder()
+                .yearMonth(getPreviousMonth())
+                .holidays(holidays)
+                .build();
+        List<OverTimeResponse> overTime = memberService.getOverTime(build);
         getExcel(overTime,response);
     }
 
-    public void getExcel(Integer holidays, HttpServletResponse response){
-        List<OverTimeResponse> overTime = memberService.getOverTime(getPreviousMonth(), holidays);
+//    public void getExcel(Integer holidays, HttpServletResponse response){
+//        OverTimeRequest build = OverTimeRequest.builder()
+//                .yearMonth(getPreviousMonth())
+//                .holidays(holidays)
+//                .build();
+//        List<OverTimeResponse> overTime = memberService.getOverTime(build);
+//
+//        try{
+//            String fileName = "OverTimeResult.xlsx";
+//            XSSFWorkbook workbook = new XSSFWorkbook();
+//            Sheet sheet = workbook.createSheet("초과 근무 계산결과");
+//
+//            // HTTP 응답 헤더 설정
+//            response.setContentType("application/vnd.ms-excel");
+//            response.setHeader("Content-Disposition", "attachment; filename="+fileName);
+//
+//            //헤더 생성
+//            Row headerRow = sheet.createRow(0);
+//            Field[] declaredFields = OverTimeResponse.class.getDeclaredFields();
+//
+//            for(int i=0;i<declaredFields.length;i++){
+//                Cell headerCell = headerRow.createCell(i);
+//                headerCell.setCellValue(declaredFields[i].getName());
+//            }
+//
+//            //데이터 생성
+//            for(int i=0;i<overTime.size();i++){
+//                OverTimeResponse overTimeResponse = overTime.get(i);
+//                Row row = sheet.createRow(i + 1);
+//
+//                for(int j=0;j<declaredFields.length;j++){
+//                    Cell cell = row.createCell(j);
+//                    Field field = declaredFields[j];
+//                    field.setAccessible(true);
+//                    Object value;
+//                    try{
+//                        value = field.get(overTimeResponse); // 필드의 값을 가져옴
+//                        if (value != null) {
+//                            cell.setCellValue(value.toString());
+//                        }else{
+//                            cell.setCellValue(0);
+//                        }
+//                    }catch (IllegalAccessException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//            FileOutputStream fileOut = new FileOutputStream(fileName);
+//            try(OutputStream out = response.getOutputStream()){
+//                workbook.write(out);
+//            }
+////            workbook.write(fileOut);
+//            workbook.close();
+//            System.out.println("엑셀 파일이 저장되었습니다");
+//
+//
+//        }
+//        catch (Exception e){
+//            System.out.println("엑셀 파일 저장 중 오류가 발생했습니다.");
+//            e.printStackTrace();
+//        }
+//
+//    }
 
-        try{
-            String fileName = "OverTimeResult.xlsx";
-            XSSFWorkbook workbook = new XSSFWorkbook();
-            Sheet sheet = workbook.createSheet("초과 근무 계산결과");
-
-            // HTTP 응답 헤더 설정
-            response.setContentType("application/vnd.ms-excel");
-            response.setHeader("Content-Disposition", "attachment; filename="+fileName);
-
-            //헤더 생성
-            Row headerRow = sheet.createRow(0);
-            Field[] declaredFields = OverTimeResponse.class.getDeclaredFields();
-
-            for(int i=0;i<declaredFields.length;i++){
-                Cell headerCell = headerRow.createCell(i);
-                headerCell.setCellValue(declaredFields[i].getName());
-            }
-
-            //데이터 생성
-            for(int i=0;i<overTime.size();i++){
-                OverTimeResponse overTimeResponse = overTime.get(i);
-                Row row = sheet.createRow(i + 1);
-
-                for(int j=0;j<declaredFields.length;j++){
-                    Cell cell = row.createCell(j);
-                    Field field = declaredFields[j];
-                    field.setAccessible(true);
-                    Object value;
-                    try{
-                        value = field.get(overTimeResponse); // 필드의 값을 가져옴
-                        if (value != null) {
-                            cell.setCellValue(value.toString());
-                        }else{
-                            cell.setCellValue(0);
-                        }
-                    }catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            FileOutputStream fileOut = new FileOutputStream(fileName);
-            try(OutputStream out = response.getOutputStream()){
-                workbook.write(out);
-            }
-//            workbook.write(fileOut);
-            workbook.close();
-            System.out.println("엑셀 파일이 저장되었습니다");
-
-
-        }
-        catch (Exception e){
-            System.out.println("엑셀 파일 저장 중 오류가 발생했습니다.");
-            e.printStackTrace();
-        }
-
-    }
-
-    public void getExcel(List object, HttpServletResponse response){
+    private void getExcel(List object, HttpServletResponse response){
 
         try{
             String fileName = "OverTimeResult.xlsx";
@@ -136,8 +146,6 @@ public class ExcelService {
 //            workbook.write(fileOut);
             workbook.close();
             System.out.println("엑셀 파일이 저장되었습니다");
-
-
         }
         catch (Exception e){
             System.out.println("엑셀 파일 저장 중 오류가 발생했습니다.");
@@ -146,9 +154,8 @@ public class ExcelService {
 
     }
 
-    private String getPreviousMonth() {
+    private YearMonth getPreviousMonth() {
         LocalDate previousMonth = LocalDate.now().minusMonths(1);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
-        return previousMonth.format(formatter);
+        return YearMonth.from(previousMonth);
     }
 }
